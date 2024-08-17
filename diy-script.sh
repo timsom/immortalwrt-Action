2daa4f98dd9452411877c6e0c06f80d02d300ce0
oppen321 <zj18139624826@gmail.com>
2024-08-17 23:56:06 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -3,6 +3,9 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
+# 更改 Argon 主题背景
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
 # 移除冲突文件
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
