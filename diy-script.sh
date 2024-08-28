8c7844f73fbb5850499999f6f192673b28c1c97f
oppen321 <zj18139624826@gmail.com>
2024-08-29 01:14:18 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -7,10 +7,16 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 cp -f $GITHUB_WORKSPACE/images/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # 移除冲突文件
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing-box*,smartdns,brook*,chinadns-ng,*dns2socks*,dns2tcp*shadowsocks-libev,*shadowsocks-rust,*simple-obfs,*tcping,*trojan-go.*trojan,*trojan-plus,*tuic-client}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-argon
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-passwall
+
+
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
