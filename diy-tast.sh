5b783758e59e34ff73ea8b0f083bb73f4f9a4b5f
oppen321 <zj18139624826@gmail.com>
2024-09-02 19:04:51 +0800

    Update diy-tast.sh

--- diy-tast.sh
+++ diy-tast.sh
@@ -1,1 +1,42 @@
+#!/bin/bash
 
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+# 更改 Argon 主题背景
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
+# 移除冲突文件
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing-box*,smartdns,brook*,chinadns-ng,*dns2socks*,dns2tcp*shadowsocks-libev,*shadowsocks-rust,*simple-obfs,*tcping,*trojan-go.*trojan,*trojan-plus,*tuic-client,*hysteria}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-argon
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-passwall
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 科学上网插件
+git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
+git_sparse_clonehttps://github.com/morytyann/OpenWrt-mihomo.git
+git clone https://github.com/Thaolga/luci-app-nekoclash  package/luci-app-nekoclash
+
+# 更换golong 1.22
+
+
+# 主题壁纸
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+./scripts/feeds update -a
+./scripts/feeds install -a
